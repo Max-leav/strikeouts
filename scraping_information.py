b344dbcc755467ff6a623bd2245d516195c60f41
Max-Leav <maxleavitt20@gmail.com>
2026-07-25 18:57:53 -0400

    added 1st stage of scraping probable pitchers

---
+++ scraping_information.py
@@ -0,0 +1,9 @@
+headers = {
+  'sec-ch-ua-platform': '"Windows"',
+  'Referer': 'https://www.fangraphs.com/players/eury-perez/27768/stats/pitching',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36 OPR/133.0.0.0 (Edition std-1)',
+  'sec-ch-ua': '"Opera GX";v="133", "Chromium";v="149", "Not)A;Brand";v="24"',
+  'sec-ch-ua-mobile': '?0'
+}
+
+fg_PitchersBaseUrl = "https://www.fangraphs.com/_next/data/Srtn7z-JINs_4qWLRn9y2/players/eury-perez/27768/stats/pitching.json?playerNameRoute=eury-perez&playerId=27768"
